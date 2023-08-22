a32510e024fbcdbf3c3922c381e4c913f142d304
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2023-08-22 09:50:54 +0900

    [level 1] Title: 모의고사, Time: 2.40 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,23 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         int[] a= {1,2,3,4,5};
         int[] b= {2,1,2,3,2,4,2,5};
         int[] c= {3,3,1,1,2,2,4,4,5,5};
-        int a_cnt =0, b_cnt =0, c_cnt =0;
+
+        int[] cnt = new int[3];
 
         for(int i=0; i< answers.length;i++){
-            if(a[i % 5] == answers[i])a_cnt++;
-            if(b[i % 8] == answers[i])b_cnt++;
-            if(c[i % 10] == answers[i])c_cnt++;
+            if(a[i % 5] == answers[i])cnt[0]++;
+            if(b[i % 8] == answers[i])cnt[1]++;
+            if(c[i % 10] == answers[i])cnt[2]++;
         }
 
-        int _max = Math.max(a_cnt, Math.max(b_cnt , c_cnt));
+        int _max = Math.max(cnt[0], Math.max(cnt[1] , cnt[2]));
         List<Integer> list = new ArrayList<>();
-        if(_max == a_cnt)list.add(1);
-        if(_max == b_cnt)list.add(2);
-        if(_max == c_cnt)list.add(3);
+        for(int i=0; i< 3; i++){
+            if(_max == cnt[i]) 
+                list.add(i+1);
+        }
 
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
