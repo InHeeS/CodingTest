bd1063cae9d95e0032e2f510002bd3e7f1aea186
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-03-05 13:23:45 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 55.11 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+import java.util.Queue;
+class Solution {
+    public static int solution(int[] queue1, int[] queue2) {
+        int answer = -1;
+
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        long sum1 = 0;
+        long sum2 = 0;
+
+        for (int n : queue1) {
+            q1.add(n);
+            sum1 += n;
+        }
+        for (int n : queue2) {
+            q2.add(n);
+            sum2 += n;
+        }
+
+        int cnt =0;
+        while (cnt < queue1.length * 4) {
+            if(sum1 > sum2){
+                int poll = q1.poll();
+                sum1 -= poll;
+                sum2 += poll;
+                q2.add(poll);
+            }else if(sum1 < sum2){
+                int poll = q2.poll();
+                sum2 -= poll;
+                sum1 += poll;
+                q1.add(poll);
+            }else if(sum1 == sum2){
+                answer = cnt;
+                break;
+            }
+            cnt++;
+        }
+
+        return answer;
+    }
+}
