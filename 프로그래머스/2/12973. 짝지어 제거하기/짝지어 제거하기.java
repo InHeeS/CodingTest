e30780f037310b3f6ae25bec1492a33ec8b80dd6
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-04-17 23:07:46 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.63 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution
+{
+    public static int solution(String s)
+    {
+        char[] ch = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : ch) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+            if (stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
