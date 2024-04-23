0664ade96473c702f3e2a61d8978d1835eb23799
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-04-23 16:33:58 +0900

    [level 2] Title: [1차] 캐시, Time: 1.93 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public static int solution(int cacheSize, String[] cities) {
+        List<String> cache = new ArrayList<>();
+        int answer = 0;
+
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toUpperCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                answer+=1;
+            }else {
+                if (cache.size() == cacheSize) {
+                    cache.remove(0);
+                }
+
+                cache.add(city);
+                answer+=5;
+            }
+
+        }
+        return answer;
+
+    }
+}
