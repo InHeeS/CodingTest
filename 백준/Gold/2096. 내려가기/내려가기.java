ccfd78bdf0caa1964d6d2e1cb79001bd835cc7dd
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-08-01 15:12:01 +0900

    [Gold V] Title: 내려가기, Time: 1244 ms, Memory: 253668 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n+1][5];
+        int[][] dmx = new int[n+1][5];
+        int[][] dmn = new int[n+1][5];
+        final int MAX_VALUE = 1000000;
+
+        for (int i = 1; i < n + 1; i++) {
+            dmx[i][0] = 0;
+            dmn[i][0] = MAX_VALUE;
+            dmx[i][4] = 0;
+            dmn[i][4] = MAX_VALUE;
+
+            for (int j = 1; j < 4; j++) {
+                arr[i][j] = sc.nextInt();
+                dmx[i][j] = arr[i][j] + Math.max(dmx[i -1][j-1], Math.max(dmx[i-1][j], dmx[i -1][j +1]));
+                dmn[i][j] = arr[i][j] + Math.min(dmn[i -1][j-1], Math.min(dmn[i-1][j], dmn[i -1][j +1]));
+            }
+        }
+        int max = Math.max(dmx[n][1], Math.max(dmx[n][2], dmx[n][3]));
+        int min = Math.min(dmn[n][1], Math.min(dmn[n][2], dmn[n][3]));
+
+        System.out.println(max +  " " + min);
+
+    }
+
+
+
+
+
+
+
+}
