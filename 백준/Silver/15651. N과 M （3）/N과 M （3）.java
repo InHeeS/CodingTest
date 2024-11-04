926c0a8f5447b988823f7afd13b654ba06a7db50
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-11-04 13:53:43 +0900

    [Silver III] Title: N과 M (3), Time: 460 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        selected = new int[M + 1];
+
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+    private static void rec_func(int k) {
+
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++)sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand <= N; cand++) {
+                selected[k] = cand;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+}
