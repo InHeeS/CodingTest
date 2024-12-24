704b168689f5dcde710ab82a2e7a24a8f891f529
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2024-12-24 13:35:59 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1336 ms, Memory: 262772 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,48 +1,52 @@
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import javax.lang.model.type.ArrayType;
 
 public class Main {
 
-    static int[] like;
-    static int[] parents;
-    static List<Integer>[] tree;
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        parents = new int[n +1];
-        like = new int[n+1];
-        tree = new ArrayList[n+ 1];
-        for (int i = 1; i <= n; i++) {
-            tree[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= n; i++) {
-            parents[i] = sc.nextInt();
-            if (parents[i] == -1)continue;
-            tree[parents[i]].add(i);
+        input();
+        pro();
+    }
 
-        }
-        for (int i = 0; i < m; i++) {
-            int employee = sc.nextInt();
-            int point = sc.nextInt();
-            like[employee] += point;
-        }
-
-        next(1);
-
+    private static void pro() {
+        dfs(1);
         for (int i = 1; i <= n ; i++) {
-            System.out.print(like[i] + " ");
+            sb.append(dy[i]).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    private static void dfs(int x) {
+        for (int y : children[x]) {
+            dy[y] += dy[x];
+            dfs(y);
         }
     }
 
-    public static void next(int node){
-        for (int child : tree[node]) {
-            like[child] += like[node];
-            next(child);
+    static StringBuilder sb = new StringBuilder();
+    static Scanner sc = new Scanner(System.in);
+    static int n, m;
+    static ArrayList<Integer>[] children;
+    static int[] dy;
+
+    private static void input() {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        dy = new int[n + 1];
+        children = new ArrayList[n + 1];
+        for (int i = 1; i <= n ; i++) {
+            children[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= n ; i++) {
+            int par = sc.nextInt();
+            if (i == 1)continue;
+            children[par].add(i);
+        }
+        for (int i = 1; i <=m ; i++) {
+            int x = sc.nextInt(), v = sc.nextInt();
+            dy[x] += v;
+
         }
     }
 
