5fa8e210bfd0374d495d9e4dad66d56c0dd9acfa
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2025-01-19 16:34:51 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT id, email, first_name, last_name
+FROM developers a
+         JOIN skillcodes b
+              ON a.skill_code & b.CODE = b.CODE
+WHERE CATEGORY = 'Front End'
+ORDER BY 1
