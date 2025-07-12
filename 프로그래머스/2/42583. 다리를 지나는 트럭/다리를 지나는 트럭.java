6f332ae2a87101575a80ca6a69661e01493dbdf3
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2025-07-12 21:08:08 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 27.95 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,33 +1,37 @@
 import java.util.LinkedList;
 import java.util.Queue;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
         Queue<Integer> bridge = new LinkedList<>();
-        // 맨처음 비어 잇는 다리 공간 모두를 0으로 초기화
         for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
+            bridge.add(0);
         }
-        if (bridge_length == 1)return truck_weights.length + 1;
-        if (truck_weights.length == 1)return bridge_length + 1;
 
-        int idx =0;
-        // 현재 다리에 있는 모든 트럭의 무게
-        int curWeight =0;
-        while(idx < truck_weights.length){
-            // 맨 앞 트럭의 무게를 빼준다.
-            curWeight -= bridge.poll();
-            answer++;
+        int time = 0;
+        int sum = 0; // 다리위의 트럭들의 무게 합 
+        int idx = 0;
 
-            if (curWeight + truck_weights[idx] <= weight){
-                bridge.offer(truck_weights[idx]);
-                curWeight += truck_weights[idx++];
+        while (idx < truck_weights.length) {
+            // 1초 경과: 맨 앞 칸(큐의 head) 비우기 → 다리 위에서 빠져나간 트럭(또는 0) 제거
+            time++;
+            sum -= bridge.poll();
+
+            if (sum + truck_weights[idx] <= weight) {
+                bridge.add(truck_weights[idx]);
+                sum += truck_weights[idx];
+                idx++;
+            } else {
+                bridge.add(0);
             }
-            //견딜 수 있는 무게보다 커진다면 0 을 넣는다.
-            else
-                bridge.offer(0);
         }
 
-        return answer + bridge_length;
+        // 마지막 트럭이 다리를 완전히 건널 때까지 남은 큐 비우기
+        while (sum > 0) {
+            time++;
+            sum -= bridge.poll();
+        }
+
+        return time;
     }
 }
