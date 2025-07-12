5c10ae059e25b98e7548906bf5a5b78284ad7f3d
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2025-07-12 18:19:44 +0900

    [level 2] Title: 기능개발, Time: 3.81 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,21 +5,28 @@ import java.util.List;
 import java.util.Queue;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> ans = new ArrayList<>();
+        /**
+         * deque
+         *
+         */
         Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < progresses.length; i++) {
-            queue.add((int)Math.ceil((100.0 - progresses[i])/speeds[i]));
+            // 남은 일수 만약 3.4 라면 4일
+            queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
         }
+
         while(!queue.isEmpty()){
-            int cnt =1;
+            int cnt = 1;
             int poll = queue.poll();
             while(!queue.isEmpty() && queue.peek() <= poll){
                 queue.poll();
                 cnt++;
             }
-            ans.add(cnt);
+            result.add(cnt);
         }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
