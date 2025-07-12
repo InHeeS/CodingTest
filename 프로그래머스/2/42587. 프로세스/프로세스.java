031ee09bbeb31ee1e9e9f62b531cc36c444174b5
Inhee Cho <105423951+InHeeS@users.noreply.github.com>
2025-07-12 19:58:32 +0900

    [level 2] Title: 프로세스, Time: 2.65 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,26 +1,46 @@
-import java.util.Collections;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
+
 class Solution {
-    public int solution(int[] priorities, int location) {
-        int ans = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+    static class Process{
+        int index;
+        int priority;
 
-        for (int priority : priorities) {
-            queue.offer(priority);
+        public Process(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
         }
+    }
 
+    public int solution(int[] priorities, int location) {
+        Deque<Process> queue = new ArrayDeque<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+
+        for (int i = 0; i < priorities.length; i++) {
+            // 주어진 큐대로 -> addLast()
+            queue.addLast(new Process(i, priorities[i]));
+            maxHeap.add(priorities[i]);
+    }
+
+        int executeCount = 0;
         while(!queue.isEmpty()){
-            for (int i = 0; i < priorities.length; i++) {
-                if(queue.peek() == priorities[i]){
-                    queue.poll();
-                    ans++;
-
-                    if(location == i){
-                        return ans;
-                    }
+            Process cur = queue.removeFirst();
+            if (cur.priority == maxHeap.peek()){
+                executeCount++;
+                maxHeap.poll();
+                if (cur.index == location){
+                    return executeCount;
                 }
+            }else{
+                queue.addLast(cur);
             }
         }
-        return ans;
+        return executeCount;
     }
 }
